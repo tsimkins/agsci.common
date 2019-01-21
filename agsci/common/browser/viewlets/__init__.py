9e4c80c57162b8e533fb51e449c932ff66da3c4a
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2019-01-21 14:49:43 -0500

    Added social media icons.

--- agsci/common/browser/viewlets/__init__.py
+++ agsci/common/browser/viewlets/__init__.py
@@ -4,22 +4,25 @@ from zope.component.hooks import getSite
 import untangle
 
 from agsci.common.utilities import ploneify
+from agsci.common import object_factory
 
 class ViewletBase(_ViewletBase):
 
     @property
     def site(self):
         return getSite()
-    
+
 class NavigationViewlet(ViewletBase):
-    
+
+    xml_file = '++resource++agsci.common/navigation/navigation.xml'
+
     def label(self, title=''):
         return "-".join([ploneify(self.nav_id), ploneify(title)])
-    
+
     @property
     def config(self):
 
-        resource = self.site.restrictedTraverse('++resource++agsci.common/navigation/navigation.xml')
+        resource = self.site.restrictedTraverse(self.xml_file)
         xml = untangle.parse(resource.context.path)
         return xml.config
 
@@ -29,11 +32,17 @@ class NavigationViewlet(ViewletBase):
         for nav in self.config.nav:
             if nav['id'] == self.nav_id:
                 return nav
-      
+
 class PrimaryNavigationViewlet(NavigationViewlet):
-    
+
     nav_id = 'primary'
 
 class AudienceNavigationViewlet(NavigationViewlet):
-    
+
     nav_id = 'audience'
+
+class SocialFooterViewlet(NavigationViewlet):
+
+    xml_file = '++resource++agsci.common/navigation/social.xml'
+
+    nav_id = 'social'
