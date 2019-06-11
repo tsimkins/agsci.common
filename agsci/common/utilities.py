3ed38ab336595a3f501dd2309e00bfc273381eb7
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2019-06-11 15:39:02 -0400

    Added a 'setSiteURL' method so we can tell the site what the URL is for printing URLs.

--- agsci/common/utilities.py
+++ agsci/common/utilities.py
@@ -200,3 +200,25 @@ def toBool(_):
     if isinstance(_, int):
         return bool(_)
     return False
+
+# This makes the 'getURL' and 'absolute_url', etc. methods return the proper
+# URL through the debug prompt.
+def setSiteURL(site, domain='trs22.psu.edu:5051', path='', https=True):
+
+    if path and not path.startswith('/'):
+        path = '/%s' % path
+
+    if https:
+        url = 'https://%s%s' % (domain, path)
+    else:
+        url = 'http://%s%s' % (domain, path)
+
+    if url.endswith('/'):
+        url = url[:-1]
+
+    site.REQUEST['SERVER_URL'] = url
+
+    site.REQUEST.other['VirtualRootPhysicalPath'] = site.getPhysicalPath()
+
+    if site.REQUEST.get('_ec_cache', None):
+        site.REQUEST['_ec_cache'] = {}
