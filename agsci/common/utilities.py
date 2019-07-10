63f8b267718cc2d4f01d65b890bbaa470506ce59
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2019-07-10 10:40:23 -0400

    Added a utility to execute methods under elevated privileges anonymously.

--- agsci/common/utilities.py
+++ agsci/common/utilities.py
@@ -6,6 +6,10 @@ from plone.dexterity.interfaces import IDexterityFTI
 from plone.i18n.normalizer import idnormalizer, filenamenormalizer
 from zope.component import getUtility
 from zope.schema.interfaces import IVocabularyFactory
+from zope.component.hooks import getSite
+from AccessControl import getSecurityManager
+from AccessControl.SecurityManagement import newSecurityManager, setSecurityManager
+from AccessControl.User import UnrestrictedUser as BaseUnrestrictedUser
 
 import htmlentitydefs
 import pytz
@@ -364,3 +368,59 @@ def scrub_html(html):
 
     # Return of scrubbed HTML
     return html
+
+class UnrestrictedUser(BaseUnrestrictedUser):
+    """Unrestricted user that still has an id.
+    """
+    def getId(self):
+        """Return the ID of the user.
+        """
+        return self.getUserName()
+
+def execute_under_special_role(roles, function, *args, **kwargs):
+    """ Execute code under special role privileges.
+
+    Example how to call::
+
+        execute_under_special_role(portal, "Manager",
+            doSomeNormallyNotAllowedStuff,
+            source_folder, target_folder)
+
+
+    @param portal: Reference to ISiteRoot object whose access controls we are using
+
+    @param function: Method to be called with special privileges
+
+    @param roles: User roles for the security context when calling the privileged code; e.g. "Manager".
+
+    @param args: Passed to the function
+
+    @param kwargs: Passed to the function
+    """
+
+    portal = getSite()
+    sm = getSecurityManager()
+
+    try:
+        try:
+            # Clone the current user and assign a new role.
+            # Note that the username (getId()) is left in exception
+            # tracebacks in the error_log,
+            # so it is an important thing to store.
+            tmp_user = UnrestrictedUser(
+                sm.getUser().getId(), '', roles, ''
+                )
+
+            # Wrap the user in the acquisition context of the portal
+            tmp_user = tmp_user.__of__(portal.acl_users)
+            newSecurityManager(None, tmp_user)
+
+            # Call the function
+            return function(*args, **kwargs)
+
+        except:
+            # If special exception handlers are needed, run them here
+            raise
+    finally:
+        # Restore the old security manager
+        setSecurityManager(sm)
