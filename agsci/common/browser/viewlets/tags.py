67b5100fec1eaa74d5e964a1d95d4af332f345a4
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2019-08-07 11:13:42 -0400

    Added a tag viewlet instead of a portlet.

---
+++ agsci/common/browser/viewlets/tags.py
@@ -0,0 +1,37 @@
+from agsci.common import object_factory
+from agsci.common.interfaces import ITagsAdapter
+
+from . import ViewletBase
+
+class TagsViewlet(ViewletBase):
+
+    target_view = "tags"
+
+    @property
+    def adapted(self):
+        return ITagsAdapter(self.context)
+
+    @property
+    def tags(self):
+        return self.adapted.selected_tags
+
+    @property
+    def tag_data(self):
+        return [
+            object_factory(
+                key=x[0],
+                value=x[1]
+            ) for x in self.tags
+        ]
+
+    @property
+    def parent_object(self):
+        return self.adapted.parent_object
+
+    @property
+    def tag_root(self):
+        return self.adapted.tag_root
+
+    @property
+    def available(self):
+        return not not self.tags
