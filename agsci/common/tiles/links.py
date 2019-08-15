3d8163e9f95af73fb5f6666059e691103cd69a90
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2019-08-15 12:40:45 -0400

    Added a check for links in a text field of a tile.

--- agsci/common/tiles/links.py
+++ agsci/common/tiles/links.py
@@ -3,10 +3,10 @@ from bs4 import BeautifulSoup
 from agsci.common import object_factory
 
 class LinkReport(object):
-    
+
     def __init__(self, context):
         self.context = context
-    
+
     def __call__(self):
         _name = self.context.__name__
         _tile_type = self.context.tile_type
@@ -19,11 +19,50 @@ class LinkReport(object):
                 url=_url,
             )
 
-class TextLinkReport(object):
-    
+class TextLinkReport(LinkReport):
+
+    @property
+    def text(self):
+
+        _ = self.context.get_field('text')
+
+        if _ and hasattr(_, 'raw'):
+            yield _.raw
+
+    @property
+    def soup(self):
+        return [BeautifulSoup(x, features="lxml") for x in self.text]
+
     @property
     def links(self):
-        pass
+
+        for soup in self.soup:
+
+            for a in soup.findAll('a'):
+
+                url = a.get('href', '')
+                label = a.text
+
+                if url:
+                    yield (label, url)
+
+class ValueTextLinkReport(TextLinkReport):
+
+    @property
+    def text(self):
+
+        value = self.context.value
+
+        if isinstance(value, (list, tuple)):
+
+            for _ in value:
+
+                if isinstance(_, (dict,)):
+
+                    text = _.get('text', None)
+
+                    if text:
+                        yield text
 
 class TitleLinkReport(LinkReport):
 
@@ -33,7 +72,7 @@ class TitleLinkReport(LinkReport):
     def links(self):
 
         title = self.context.get_field(self.title_field, 'N/A')
-        url = self.context.get_field('url', None)                
+        url = self.context.get_field('url', None)
 
         if url:
             yield (title, url)
@@ -48,9 +87,9 @@ class CTALinkReport(LinkReport):
 
     @property
     def links(self):
-    
+
         value = self.context.value
-    
+
         if isinstance(value, (list, tuple)):
 
             for _ in value:
@@ -58,7 +97,7 @@ class CTALinkReport(LinkReport):
                 if isinstance(_, (dict,)):
 
                     title = _.get(self.title_field, 'N/A')
-                    url = _.get('url', None)                
+                    url = _.get('url', None)
 
                     if url:
                         yield (title, url)
@@ -76,21 +115,21 @@ def link_factory(_):
 
     return {
         'agsci.common.tiles.animal' : [],
-        'agsci.common.tiles.callout_block' : [],
+        'agsci.common.tiles.callout_block' : [TextLinkReport,],
         'agsci.common.tiles.cta' : [ButtonLinkReport, ],
         'agsci.common.tiles.dropdown_accordion' : [],
         'agsci.common.tiles.explore_more' : [],
         'agsci.common.tiles.fozziebear' : [],
-        'agsci.common.tiles.gonzo' : [TitleLinkReport,],
+        'agsci.common.tiles.gonzo' : [TitleLinkReport, TextLinkReport,],
         'agsci.common.tiles.jumbotron' : [],
         'agsci.common.tiles.kermit' : [ButtonLinkReport, ],
-        'agsci.common.tiles.large_cta' : [LabelLinkReport,],
-        'agsci.common.tiles.misspiggy' : [],
+        'agsci.common.tiles.large_cta' : [LabelLinkReport, TextLinkReport],
+        'agsci.common.tiles.misspiggy' : [TextLinkReport,],
         'agsci.common.tiles.navigation' : [],
         'agsci.common.tiles.pepe_the_king_prawn' : [],
         'agsci.common.tiles.portlets' : [],
         'agsci.common.tiles.pull_quote' : [],
-        'agsci.common.tiles.richtext' : [],
+        'agsci.common.tiles.richtext' : [TextLinkReport,],
         'agsci.common.tiles.rizzo_the_rat' : [],
         'agsci.common.tiles.rowlf' : [LabelLinkReport,],
         'agsci.common.tiles.scooter' : [],
