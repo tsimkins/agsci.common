3c57060e973bb571eef5c46111bedf5ba3d4202d
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2021-12-06 13:41:06 -0500

    WF 1101774 Tweaking RSS feed output.

--- agsci/common/patch/__init__.py
+++ agsci/common/patch/__init__.py
@@ -1,4 +1,7 @@
 from plone.app.widgets.base import TextareaWidget
+from DateTime import DateTime
+
+from agsci.common.content.behaviors.leadimage import ILeadImage
 
 def textarea_autocomplete(self, pattern, pattern_options={}, name=None, value=None):
 
@@ -13,3 +16,31 @@ def textarea_autocomplete(self, pattern, pattern_options={}, name=None, value=No
         self.name = name
     if value is not None:
         self.value = value
+
+def syndication_start(self):
+    if hasattr(self.context, 'start'):
+        date = self.context.start
+        if date and date != 'None':
+            return DateTime(date)
+
+def syndication_public_tags(self):
+    tags = getattr(self.context, 'public_tags', [])
+    if tags and isinstance(tags, (list, tuple)):
+        return list(tags)
+
+def syndication_enclosure(self):
+    lead = ILeadImage(self.context, None)
+    if lead:
+        if (lead.image
+            and hasattr(lead.image, 'getSize')
+            and lead.image.getSize() > 0):
+            return u"""
+                <enclosure
+                    url="%s"
+                    length="%d"
+                    type="%s" />
+            """ % (
+                '%s/@@images/image/large' % self.context.absolute_url(),
+                lead.image.getSize(),
+                lead.image.contentType,
+            )
