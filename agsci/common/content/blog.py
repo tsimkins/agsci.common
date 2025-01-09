c0adb1a86fa2e5a100fb17ed46408f7f770e561a
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2025-01-09 09:46:35 -0500

    Added an auto-expiration configuration at the blog level, and an event that sets the expiration date based on that on publish/submit.

--- agsci/common/content/blog.py
+++ agsci/common/content/blog.py
@@ -1,11 +1,27 @@
 from plone.autoform.interfaces import IFormFieldProvider
 from plone.dexterity.content import Container
 from plone.supermodel import model
+from zope import schema
 from zope.interface import provider
+
+from agsci.common import AgsciMessageFactory as _
 
 @provider(IFormFieldProvider)
 class IBlog(model.Schema):
-    pass
+
+    auto_expire = schema.Bool(
+        title=_(u"Auto-Expire News Items"),
+        description=_(u"Automatically set expiration date for news items inside this blog."),
+        required=False,
+        default=True,
+    )
+
+    auto_expire_months = schema.Int(
+        title=_(u"Auto-Expire News Item Months"),
+        description=_(u"Number of months to keep news items."),
+        required=False,
+        default=60,
+    )
 
 class Blog(Container):
     pass
