91c617de20bece5776ad8f52fbdd6372e8ae729e
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2025-01-21 13:59:29 -0500

    Adding 'global' as a valid tag.

--- agsci/common/load/news/__init__.py
+++ agsci/common/load/news/__init__.py
@@ -169,6 +169,7 @@ class ImportNewsView(ImportContentView):
 
         # Tags (excluding news-)
         tags = [
+            'global',
             'research',
             'student-stories',
             'students',
