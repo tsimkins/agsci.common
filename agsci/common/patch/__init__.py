6932228dc00d807d1ecb6b0dbee39ae8aa789ba8
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2025-04-09 13:48:03 -0400

    Adding monkeypatch to show toolbar so people can copy content they can't edit

--- agsci/common/patch/__init__.py
+++ agsci/common/patch/__init__.py
@@ -3,8 +3,11 @@ try:
 except:
     from plone.app.widgets.base import TextareaWidget
 
+from Acquisition import aq_inner
 from DateTime import DateTime
+from Products.CMFCore.utils import getToolByName
 from xml.sax.saxutils import quoteattr
+from zope.component import getMultiAdapter
 
 from agsci.common.content.behaviors.leadimage import ILeadImage
 
@@ -78,3 +81,65 @@ def image_tag_from_values(*values):
     parts.append("/>")
 
     return " ".join(parts)
+
+def ploneview_showToolbar(self):
+    """Determine if the editable border should be shown"""
+    request = self.request
+    if "disable_border" in request or "disable_toolbar" in request:
+        return False
+    if "enable_border" in request or "enable_toolbar" in request:
+        return True
+
+    context = aq_inner(self.context)
+
+    portal_membership = getToolByName(context, "portal_membership")
+    checkPerm = portal_membership.checkPermission
+
+    if (
+        checkPerm("Modify portal content", context)
+        or checkPerm("Add portal content", context)
+        or checkPerm("Review portal content", context)
+        or checkPerm("Copy or Move", context)
+    ):
+        return True
+
+    if portal_membership.isAnonymousUser():
+        return False
+
+    context_state = getMultiAdapter((context, request), name="plone_context_state")
+    actions = context_state.actions
+
+    if actions("workflow", max=1):
+        return True
+
+    if actions("batch", max=1):
+        return True
+
+    for action in actions("object"):
+        if action.get("id", "") != "view":
+            return True
+
+    template_id = None
+    if "PUBLISHED" in request:
+        if getattr(request["PUBLISHED"], "getId", None):
+            template_id = request["PUBLISHED"].getId()
+
+    idActions = {}
+    for obj in actions("object") + actions("folder"):
+        idActions[obj.get("id", "")] = 1
+
+    if "edit" in idActions:
+        if template_id in idActions or template_id in [
+            "synPropertiesForm",
+            "folder_contents",
+            "folder_listing",
+            "listing_view",
+        ]:
+            return True
+
+    # Check to see if the user is able to add content
+    allowedTypes = context.allowedContentTypes()
+    if allowedTypes:
+        return True
+
+    return False
