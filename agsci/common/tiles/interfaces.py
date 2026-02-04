9766ea4aae88571d785324e24a533aefaf56ac10
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2026-02-04 15:37:08 -0500

    Added iframe alt text to Miss Piggy

--- agsci/common/tiles/interfaces.py
+++ agsci/common/tiles/interfaces.py
@@ -384,6 +384,13 @@ class IMissPiggyTile(IBorderTile):
         required=False,
     )
 
+    iframe_alt = schema.TextLine(
+        title=_(u"Alternative text for video"),
+        description=_(u"Used as iframe title"),
+        default="YouTube Video",
+        required=True,
+    )
+
     video_aspect_ratio = schema.Choice(
         title=_(u"Video Aspect Ratio"),
         vocabulary="agsci.common.tiles.video_aspect_ratio",
